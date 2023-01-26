728651c3a148cf7a475672de1bf724c07486c385
Oliver Alt <oa@lacheweg21.de>
2023-01-26 21:56:57 +0100

    Implementation of stream data provider interfaces

---
+++ src/MDD4All.SpecIF.DataProvider.Base/DataStreams/SpecIfStreamDataPublisherProvider.cs
@@ -0,0 +1,9 @@
+﻿using MDD4All.SpecIF.DataProvider.Contracts.DataStreams;
+
+namespace MDD4All.SpecIF.DataProvider.Base.DataStreams
+{
+    public class SpecIfStreamDataPublisherProvider : ISpecIfStreamDataPublisherProvider
+    {
+        public ISpecIfDataPublisher StreamDataPublisher { get; set; }
+    }
+}
---
+++ src/MDD4All.SpecIF.DataProvider.Base/DataStreams/SpecIfStreamDataSubscriberProvider.cs
@@ -0,0 +1,9 @@
+﻿using MDD4All.SpecIF.DataProvider.Contracts.DataStreams;
+
+namespace MDD4All.SpecIF.DataProvider.Base.DataStreams
+{
+    public class SpecIfStreamDataSubscriberProvider : ISpecIfStreamDataSubscriberProvider
+    {
+        public ISpecIfDataSubscriber StreamDataSubscriber { get; set; }
+    }
+}
